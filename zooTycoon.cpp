196887b4dbe74470238b9fbd63f0709c47b08f66
brandon <brandon.jones085@gmail.com>
2019-04-20 20:18:39 -0400

    four

--- zooTycoon.cpp
+++ zooTycoon.cpp
@@ -1,21 +1,57 @@
-// zooTycoon.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
+#include "Animal.h"
+#include "Tiger.h"
+#include "Zoo.h"
+#include "Penguin.h"
+#include "Turtle.h"
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	
+	int days = 1; 
+	int again = 1;
+
+
+
+	Animal a; 
+	a.setMoneyInBank(100000); 
+
+	int moneyLeft = a.getMoneyInBank(); 
+
+	a.setBaseFoodCost();
+	Zoo z; 
+	
+
+	while (again == 1)
+	{
+		z.startZoo(); 
+
+		std::cout << "Day number: " << days << std::endl; 
+		std::cout << "Money left in the bank: " << moneyLeft << std::endl; 
+	
+
+
+		//Pause until user presses enter
+		std::cin.clear(); 
+		std::cin.sync(); 
+		std::cin.ignore(); 
+		days++;
+
+
+		//Asks user if they'd like to continue
+		std::cout << "Enter 1 to continue playing or 2 to quit: "; 
+		std::cin >> again; 
+	}
+
+
+	
+	std::cout << "\n\nGoodbye\n\n"; 
+
+	
+
+
+	return 0; 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
